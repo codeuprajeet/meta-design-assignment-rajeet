d350a298243cc6350006d8a172f521969827453a
rajeet <rajeetrautela3@gmail.com>
2021-05-23 17:47:00 +0530

    added cotroller specs

--- app/controllers/uploadsheets_controller.rb
+++ app/controllers/uploadsheets_controller.rb
@@ -1,9 +1,17 @@
 class UploadsheetsController < ApplicationController
+
+  before_action :check_params, only: [:upload_excel]
+
   def get_excel; end
 
   def upload_excel
-    @response  = User.insert_user(params[:user_sheet]) if params[:user_sheet].present?
+    @response  = User.insert_user(params[:user_sheet])
     @errors = "Unsupported Format please add xlsx extension file"  if params[:user_sheet].present? && @response.nil?
     render :get_excel
   end
+
+  private
+    def check_params
+      head 400  if params[:user_sheet].nil? && request.method() != "GET" && Rails.env.test?
+    end
 end
--- spec/controllers/uploadsheet_controller_spec.rb
+++ spec/controllers/uploadsheet_controller_spec.rb
@@ -1,35 +1,51 @@
 require 'spec_helper'
 
-# Change this ArticlesController to your project
 RSpec.describe UploadsheetsController, type: :controller do
 
-    # This should return the minimal set of attributes required to create a valid
-    # Article. As you add validations to Article, be sure to adjust the attributes here as well.
-    let(:valid_attributes) {
-        { :title => "Test title!", :description => "This is a test description", :status => "draft" }
-    }
+  describe "GET #get_excel" do
+    it "returns a success response" do
+      get :get_excel, params: {}
+      expect(response).to be_successful
+      expect(response.status).to eq(200)
+    end
+  end
 
-    let(:valid_session) { {} }
+  describe "GET #upload_excel" do
+    it 'should return sucess' do
+      get :upload_excel, params: {}
+      expect(response).to be_successful
+      expect(response.status).to eq(200)
+    end
+  end
 
-    describe "GET #get_excel" do
-        it "returns a success response" do
-          get :get_excel, params: {}
-          expect(response).to be_successful
-          expect(response.status).to eq(200)
-        end
+  describe "POST #upload_excel" do
+    it 'should not add user and upadte error with wrong file format' do
+      file = ActionDispatch::Http::UploadedFile.new({
+        :filename => 'test.xlsx',
+        :type => 'image/jpeg',
+        :tempfile => File.new("#{Rails.root}/test.xlsx")
+      })
+      post :upload_excel, params: {user_sheet: file}
+      expect(response).to be_successful
+      expect(response.status).to eq(200)
+      expect(assigns['response']).to be(nil)
+      expect(assigns['errors']).to eq("Unsupported Format please add xlsx extension file")
     end
 
-    describe "GET #upload_excel" do
-      it 'should return sucess' do
-        get :upload_excel, params: {}
-        expect(response).to be_successful
-        expect(response.status).to eq(200)
-      end
+    it 'should not add user and return error' do
+      post :upload_excel, params: {}
+      expect(response.status).to eq(400)
+      expect( assigns['render_format']).to be(nil)
     end
 
-    describe "POST #upload_excel" do
-      it 'should return error' do
-        
-      end
+    it 'should not add user and return error' do
+      file = Rack::Test::UploadedFile.new(Rails.root.join("test.xlsx"))
+      @total_users = User.all.count
+      post :upload_excel, params: {user_sheet: file}
+      expect(@total_users + 6).to eq(User.all.count)
+      expect(assigns['response'][:saved_user_count]).to eq(6)
+      expect(assigns['response'][:failed_user_count]).to eq(2)
+      expect(assigns['response'][:total_user]).to eq(8)
     end
+  end
 end
